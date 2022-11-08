9869c6681b89ef0480838bb8932e68792eb1b64a
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-11-08 18:45:25 +0100

    Tests changed to test as a class

--- tests/test_am2301.py
+++ tests/test_am2301.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 """Tests for `am2301` package."""
-from am2301 import am2301
-
+from am2301.am2301 import AM2301 as AM2301
+import platform
+import subprocess
 
 def test_temperature_humidity():
     # Arrange
-    gpio_pin=4
+    gpio_pin=14
+    a = AM2301(gpio_pin=gpio_pin)
 
     # Act
-    temp_celsius, humidity = am2301.temperature_humidity(gpio_pin=gpio_pin)
+    temp_celsius = a.temperature
+    humidity = a.humidity
 
     # Assert
-    assert type(temp_celsius) is float
-    assert type(humidity) is float
+    assert isinstance(temp_celsius, float)
+    assert isinstance(humidity, float)
